d2c3c1c8c375dcaa3994a3a4633257494c9802f0
juanpaa777 <juandiegopardozamrripa@gmail.com>
2026-07-16 21:47:51 -0600

    config: cambiar URL de backend a IP local de depuracion 192.168.1.69

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -5,7 +5,7 @@ class Environment {
   // Resuelve la dirección IP del host según la plataforma (localhost en Windows/Web, 10.0.2.2 en Android)
   static String get apiUrl {
     if (kIsWeb) return 'http://localhost:8080/api';
-    if (Platform.isAndroid) return 'http://10.0.2.2:8080/api';
+    if (Platform.isAndroid) return 'http://192.168.1.69:8080/api';
     return 'http://localhost:8080/api';
   }
   
